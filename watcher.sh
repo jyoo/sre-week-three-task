5e7c374f86cd8314702990001578ae7683a505a7
Jung Sup (James) Yoo <37724247+jyoo@users.noreply.github.com>
2024-04-21 16:23:14 -0400

    Update watcher.sh

--- watcher.sh
+++ watcher.sh
@@ -1,1 +1,29 @@
-#TODO
+#!/bin/bash
+
+# Define Variables
+NAMESPACE="your_namespace"
+DEPLOYMENT_NAME="swype"
+MAX_RESTARTS=3
+
+# Start an infinite loop
+while true; do
+    # Get the current number of restarts for the pod
+    CURRENT_RESTARTS=$(kubectl get pods -n $NAMESPACE -l app=$DEPLOYMENT_NAME -o jsonpath='{.items[*].status.containerStatuses[*].restartCount}')
+
+    # Display the restart count
+    echo "Current restart count: $CURRENT_RESTARTS"
+
+    # Check if the restart count exceeds the maximum allowed restarts
+    if [[ $CURRENT_RESTARTS -gt $MAX_RESTARTS ]]; then
+        echo "Restart limit exceeded, scaling down the deployment..."
+        # Scale down the deployment
+        kubectl scale deployment/$DEPLOYMENT_NAME --replicas=0 -n $NAMESPACE
+        # Break the loop
+        break
+    fi
+
+    # Pause for 60 seconds
+    sleep 60
+done
+
+echo "Deployment has been scaled down."
